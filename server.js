9f9ad1e005efa5ddfdcec5f9294cd8c1cc47e637
Aubrey Ross <aubrey.ross04@gmail.com>
2022-01-21 04:27:18 -0600

    updated server.js file

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
+//Dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+//instructs which port the server will listen on
+const PORT = process.env.PORT || 3000;
 
+//initializes express
 const app = express();
 
+//middleware
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+//connects to mongoDB with the correct environment variable and connection string
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
+//starts server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
